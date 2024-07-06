20a62d4e3e917f1df8f6d6e7747354b0db3c418e
ZainUlBasit <zainulbasit486@gmail.com>
2024-07-07 00:49:59 +0500

    setup completed

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
